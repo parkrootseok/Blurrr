b445b5110ce4581b356bfd3fd1b759f689593029
전상현 <breakking@naver.com>
2024-07-17 16:18:35 +0900

    init commit

---
+++ backend/blur/src/main/java/com/luckvicky/blur/BlurApplication.java
@@ -0,0 +1,13 @@
+package com.luckvicky.blur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlurApplication.class, args);
+	}
+
+}
---
+++ backend/blur/src/main/java/com/luckvicky/blur/global/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.luckvicky.blur.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi noAuthApi() {
+        // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
+        String[] noAuthPaths = {"/login", "/test"};
+
+        return GroupedOpenApi.builder()
+                .group("no auth")  // 그룹 이름을 설정한다.
+                .pathsToMatch(noAuthPaths)     // 그룹에 속하는 경로 패턴을 지정한다.
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi authApi() {
+        String[] authPaths = {"/spots/**"};
+        return GroupedOpenApi.builder()
+                .group("auth")
+                .pathsToMatch(authPaths)
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("블러 API")
+                .description("블러 OpenAPI")
+                .version("v1");
+    }
+}
