95dc1e09123692cbd6acace17c35912fb022a011
박근석 <parkrootseok@gmail.com>
2024-07-21 20:34:06 +0900

    chore: 불 필요한 코드 제거 (#1)

--- backend/blur/src/main/java/com/luckvicky/blur/global/util/ResponseUtil.java
+++ backend/blur/src/main/java/com/luckvicky/blur/global/util/ResponseUtil.java
@@ -2,25 +2,26 @@ package com.luckvicky.blur.global.util;
 
 import com.luckvicky.blur.global.model.dto.Result;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class ResponseUtil {
 
-    public static org.springframework.http.ResponseEntity ok(Result result) {
-        return new org.springframework.http.ResponseEntity(
+    public static ResponseEntity ok(Result result) {
+        return new ResponseEntity(
                 result,
                 HttpStatus.OK
         );
     }
 
-    public static org.springframework.http.ResponseEntity noContent(Result result) {
-        return new org.springframework.http.ResponseEntity(
+    public static ResponseEntity noContent(Result result) {
+        return new ResponseEntity(
                 result,
                 HttpStatus.NO_CONTENT
         );
     }
 
-    public static org.springframework.http.ResponseEntity created(Result result) {
-        return new org.springframework.http.ResponseEntity(
+    public static ResponseEntity created(Result result) {
+        return new ResponseEntity(
                 result,
                 HttpStatus.CREATED
         );
