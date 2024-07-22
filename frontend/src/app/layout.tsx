f3367d2ed93cd9450d5b20b4cb4155e053d6f260
김주연 <juyun7908@naver.com>
2024-07-22 15:21:42 +0900

    chore: 폰트 및 메인 컬러 설정
    - 폰트 수트 적용
    - 메인컬러 주황색 적용

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,21 +1,30 @@
-import StyledComponentsRegistry from '../lib/registry'
-import Header from '@/components/common/layout/Header'
-import Footer from '@/components/common/layout/Footer'
+// src/app/layout.tsx
+"use client";
+
+import { ThemeProvider } from "styled-components";
+import StyledComponentsRegistry from "../lib/registry";
+import Header from "@/components/common/layout/Header";
+import Footer from "@/components/common/layout/Footer";
+import GlobalStyle from "../styles/GlobalStyle";
+import { theme } from "../styles/theme";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <body>
         <StyledComponentsRegistry>
-          <Header/>
-          {children}
-          <Footer/>
-        </StyledComponentsRegistry>     
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Header />
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
-  )
+  );
 }
