6be52ee4cc91455acaab3eb6f28c66441fe5edb2
parkrootseok <parkrootseok@gmail.com>
2024-07-22 13:53:09 +0900

    docs: 리그 유형별 조회 API 명세서 작성 (#3)

--- backend/blur/src/main/java/com/luckvicky/blur/domain/league/model/dto/response/LeagueListResponse.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/league/model/dto/response/LeagueListResponse.java
@@ -1,10 +1,13 @@
 package com.luckvicky.blur.domain.league.model.dto.response;
 
 import com.luckvicky.blur.domain.league.model.dto.LeagueDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(name = "리그 유형 별 조회 응답")
 public record LeagueListResponse(
+        @Schema(description = "조회한 리그 목록")
         Iterable<LeagueDto> leagues
 ) {
 
