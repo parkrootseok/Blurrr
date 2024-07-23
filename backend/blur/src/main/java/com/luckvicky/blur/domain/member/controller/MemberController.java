606fa011e0d29873ce0eade9b739716da673038e
전상현 <breakking@naver.com>
2024-07-23 09:33:49 +0900

    feat: 회원가입 API 유효성 추가
    - 비밀번호 일치 여부
    - 중복 이메일 검증
    - 이메일, 비밀번호 형식 유효성 추가
    
    
    Former-commit-id: c64979302522c62de8a1e47e7056b95e6a25ef54

--- backend/blur/src/main/java/com/luckvicky/blur/domain/member/controller/MemberController.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/member/controller/MemberController.java
@@ -1,4 +1,12 @@
 package com.luckvicky.blur.domain.member.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "유저 API", description = "사용자 관련 API")
+@RestController
+@RequestMapping("/v1/members")
 public class MemberController {
+
 }
