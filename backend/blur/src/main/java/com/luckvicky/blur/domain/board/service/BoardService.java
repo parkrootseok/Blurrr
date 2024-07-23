a827821b3768d29a9fa320009daaf81a4297f724
박근석 <parkrootseok@gmail.com>
2024-07-23 19:42:06 +0900

    refactor: 명세서 및 네이밍 수정  (#22)
    Former-commit-id: 27bfd78df485a48ebb636930ae9fbec8b8b3e83d

--- backend/blur/src/main/java/com/luckvicky/blur/domain/board/service/BoardService.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/board/service/BoardService.java
@@ -5,6 +5,9 @@ import com.luckvicky.blur.domain.board.model.dto.request.BoardCreateRequest;
 import java.util.List;
 
 public interface BoardService {
+
     Boolean createBoard(BoardCreateRequest request);
-    List<BoardDto> searchBoardByBoardType(String boardType);
+
+    List<BoardDto> findBoardByType(String boardType);
+
 }
