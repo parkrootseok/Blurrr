1fe5b494d87b6d1f352c3a8ee91d1dea41f0cc4b
박근석 <parkrootseok@gmail.com>
2024-07-24 15:52:31 +0900

    댓글 목록 조회 기능 (#29)

---
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/comment/model/dto/ReplyDto.java
@@ -0,0 +1,27 @@
+package com.luckvicky.blur.domain.comment.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.luckvicky.blur.domain.member.model.MemberDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+@Schema(name = "대댓글")
+public class ReplyDto {
+
+    @Schema(description = "고유 식별값")
+    private UUID id;
+
+    @Schema(description = "작성자 정보")
+    private MemberDto member;
+
+    @Schema(description = "내용")
+    private String content;
+
+    @Schema(description = "생성일자")
+    private String createdAt;
+
+}
--- backend/blur/src/main/java/com/luckvicky/blur/domain/comment/repository/CommentRepository.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/comment/repository/CommentRepository.java
@@ -1,11 +1,16 @@
 package com.luckvicky.blur.domain.comment.repository;
 
+import com.luckvicky.blur.domain.board.model.entity.Board;
 import com.luckvicky.blur.domain.comment.model.entity.Comment;
+import com.luckvicky.blur.domain.comment.model.entity.CommentType;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+
+    List<Comment> findAllByBoardAndType(Board board, CommentType type);
 
 }
