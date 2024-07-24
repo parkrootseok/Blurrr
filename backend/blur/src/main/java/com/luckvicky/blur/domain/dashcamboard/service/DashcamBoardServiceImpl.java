4582a0380345f2b1f12971aae666aa4343a3c4ff
최은혜 <antlf1104@naver.com>
2024-07-24 09:38:01 +0900

    feat : 블랙박스 게시물 상세 조회 (#13)
    Former-commit-id: 87fa2b5d3af67d77a2450efe39df2bf152d31ea2

--- backend/blur/src/main/java/com/luckvicky/blur/domain/dashcamboard/service/DashcamBoardServiceImpl.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/dashcamboard/service/DashcamBoardServiceImpl.java
@@ -1,13 +1,16 @@
 package com.luckvicky.blur.domain.dashcamboard.service;
 
 import com.luckvicky.blur.domain.dashcamboard.mapper.DashcamBoardMapper;
+import com.luckvicky.blur.domain.dashcamboard.model.dto.DashcamBoardDto;
 import com.luckvicky.blur.domain.dashcamboard.model.dto.DashcamBoardListDto;
 import com.luckvicky.blur.domain.dashcamboard.model.entity.Dashcam;
 import com.luckvicky.blur.domain.dashcamboard.repository.DashcamRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,12 @@ public class DashcamBoardServiceImpl implements DashcamBoardService{
     public List<DashcamBoardListDto> getDashcamBoards() {
         List<Dashcam> dashcams = dashcamRepository.findAll();
         return dashcamBoardMapper.toDashcamBoardListDtos(dashcams);
+    }
+
+    @Override
+    public DashcamBoardDto getDashcamBoardById(UUID id) {
+        Dashcam dashcam = dashcamRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Dashcam board not found with id: " + id));
+        return dashcamBoardMapper.toDashcamBoardDto(dashcam);
     }
 }
