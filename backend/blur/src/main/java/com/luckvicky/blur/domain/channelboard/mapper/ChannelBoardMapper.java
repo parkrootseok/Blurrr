f1a4593dc1f364475c3da911bdef60441edabcf9
최은혜 <antlf1104@naver.com>
2024-07-26 06:13:41 +0900

    refact : 채널 게시물 목록 조회 멘션리그 추가 (#35)
    - 게시물 목록 조회시, 멘션 리그도 함께 조회되도록 추가
    
    
    Former-commit-id: e035f566f250c7d96d6527cdf0a146d12e39124f

---
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/mapper/ChannelBoardMapper.java
@@ -0,0 +1,49 @@
+package com.luckvicky.blur.domain.channelboard.mapper;
+
+import com.luckvicky.blur.domain.board.model.dto.BoardDto;
+import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardListDto;
+import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardMentionDto;
+import com.luckvicky.blur.domain.channelboard.model.entity.ChannelBoard;
+import com.luckvicky.blur.domain.channelboard.model.entity.ChannelBoardMention;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ChannelBoardMapper {
+
+    public ChannelBoardListDto toChannelBoardListDto(ChannelBoard channelBoard, List<ChannelBoardMention> mentions) {
+        BoardDto boardDto = toBoardDto(channelBoard);
+        List<ChannelBoardMentionDto> mentionDtos = toChannelBoardMentionDtoList(mentions);
+
+        return ChannelBoardListDto.of(boardDto, mentionDtos);
+    }
+
+    private BoardDto toBoardDto(ChannelBoard channelBoard) {
+        return BoardDto.builder()
+                .id(channelBoard.getId())
+//                .member(channelBoard.getMember().toDto())
+                .title(channelBoard.getTitle())
+                .createdAt(channelBoard.getCreatedAt().toString())
+                .commentCount(channelBoard.getCommentCount())
+                .likeCount(channelBoard.getLikeCount())
+                .build();
+    }
+
+    private List<ChannelBoardMentionDto> toChannelBoardMentionDtoList(List<ChannelBoardMention> mentions) {
+        return mentions.stream()
+                .map(ChannelBoardMentionDto::of)
+                .collect(Collectors.toList());
+    }
+
+    public List<ChannelBoardListDto> toChannelBoardListDtoList(List<ChannelBoard> channelBoards, List<List<ChannelBoardMention>> mentionsList) {
+        return channelBoards.stream()
+                .map(channelBoard -> {
+                    int index = channelBoards.indexOf(channelBoard);
+                    return toChannelBoardListDto(channelBoard, mentionsList.get(index));
+                })
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/repository/ChannelBoardMentionRepository.java
@@ -0,0 +1,14 @@
+package com.luckvicky.blur.domain.channelboard.repository;
+
+import com.luckvicky.blur.domain.channelboard.model.entity.ChannelBoard;
+import com.luckvicky.blur.domain.channelboard.model.entity.ChannelBoardMention;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChannelBoardMentionRepository extends JpaRepository<ChannelBoardMention, Long> {
+    List<ChannelBoardMention> findByChannelBoard(ChannelBoard channelBoard);
+
+}
