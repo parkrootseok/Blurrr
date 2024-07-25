706b2292ddfd2ee3d7876c411393abdce120f2d8
김주연 <juyun7908@naver.com>
2024-07-25 11:27:29 +0900

    feat: 리그 글쓰기 UI 구현(#36)
    - 리그 글쓰기 UI 구현
    - 리그 메인페이지에서 글쓰기 버튼 연결
    
    
    Former-commit-id: fe6ab1f6cb9b49ee93f9a10db7dde88170e3835d

---
+++ frontend/src/app/(league)/league/[leagueId]/write/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React from "react";
+import { useSearchParams, usePathname } from "next/navigation";
+import styled from "styled-components";
+import QuillEditor from "@/components/channel/board/QuillEditor";
+import Breadcrumb from "@/components/common/UI/BreadCrumb";
+
+const Page = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const leagueName = searchParams.get("leagueName") || "";
+  const leagueId = pathname.split("/")[2];
+  console.log(leagueId);
+
+  return (
+    <Container>
+      <Title>{leagueName} 리그 게시글 작성</Title>
+      <Input placeholder="제목을 입력해주세요." />
+      <EditorContainer>
+        <QuillEditor />
+      </EditorContainer>
+      <FileInputContainer>
+        <FileInput>video.mp4</FileInput>
+        <FileInput>첨부파일</FileInput>
+      </FileInputContainer>
+      <SubmitButton>작성</SubmitButton>
+    </Container>
+  );
+};
+
+export default Page;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 16px;
+`;
+
+const BreadcrumbContainer = styled.div`
+  width: 100%; /* Full width */
+  max-width: 1000px;
+  margin-bottom: 16px; /* Space between Breadcrumb and content */
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 24px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  max-width: 800px;
+  padding: 10px;
+  margin-bottom: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-sizing: border-box; /* 추가 */
+`;
+
+const EditorContainer = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-bottom: 16px;
+  box-sizing: border-box; /* 추가 */
+`;
+
+const FileInputContainer = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-bottom: 16px;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box; /* 추가 */
+`;
+
+const FileInput = styled.div`
+  width: 100%; /* 추가 */
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  margin-bottom: 8px;
+  box-sizing: border-box; /* 추가 */
+`;
+
+const SubmitButton = styled.button`
+  width: 10%;
+  max-width: 800px;
+  padding: 12px;
+  background-color: #ddd;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+`;
