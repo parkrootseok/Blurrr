b5c36e01381c51da2f58b1b27ae811d743e0d228
조민주 <pida6716@gmail.com>
2024-07-26 16:28:35 +0900

    style: 아이콘 및 폰트 크기 수정 (#34)

--- frontend/src/app/(channel)/channels/boast/page.tsx
+++ frontend/src/app/(channel)/channels/boast/page.tsx
@@ -6,22 +6,6 @@ import BoastCard from '@/components/channel/boast/BoastCard';
 import PostTitle from '@/components/channel/PostTitle';
 import dummy from "@/db/data.json";
 
-const Container = styled.div`
-  margin: 10px 20px; /* 기본 마진 설정 */
-  
-  @media (min-width: 768px) {
-    margin: 30px 100px; /* 태블릿 크기 이상일 때 */
-  }
-
-  @media (min-width: 1024px) {
-    margin: 50px 150px; /* 데스크탑 크기 이상일 때 */
-  }
-
-  @media (min-width: 1440px) {
-    margin: 70px 300px; /* 큰 데스크탑 크기 이상일 때 */
-  }
-`;
-
 const CardGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
@@ -31,10 +15,9 @@ const CardGrid = styled.div`
 const Boast: React.FC = () => {
 
    return (
-      <Container>
+      <>
          <PostTitle
-            channel="채널"
-            subChannel="내 차 자랑"
+            channel="boast"
             title="내 차를 자랑해보자"
          />
          <CardGrid>
@@ -48,7 +31,7 @@ const Boast: React.FC = () => {
                </div>
             ))}
          </CardGrid>
-      </Container>
+      </>
    );
 };
 
