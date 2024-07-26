3a5cb7afc92df78782722f6b94e88e427955e9cb
조민주 <pida6716@gmail.com>
2024-07-26 16:13:55 +0900

    feat: 게시물에 좋아요 UI 및 토글 기능 추가 (#34)
    - 블랙박스 상세 게시글 좋아요
    - 일반 게시글 상세 좋아요
    
    
    Former-commit-id: fb83b54359a72e5cf0d32d4002b6b2de23bdcccd

--- frontend/src/components/channel/dashcam/detail/DashCamContent.tsx
+++ frontend/src/components/channel/dashcam/detail/DashCamContent.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Divider } from "@nextui-org/divider";
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
 
 const Container = styled.div`
   width: 100%;
@@ -14,10 +14,16 @@ const Container = styled.div`
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: start;
+  margin-top: 10px;
   margin-bottom: 16px;
   padding : 5px 28px;
 `;
+
+const Title = styled.div`
+   padding : 0px 28px;
+   border-bottom: 1px solid #e0e0e0;
+`
 
 const Body = styled.div`
    padding : 0px 28px;
@@ -29,8 +35,8 @@ const User = styled.div`
 `;
 
 const Avatar = styled.div`
-  width: 40px;  // 수정된 크기
-  height: 40px; // 수정된 크기
+  width: 40px;
+  height: 40px;
   border-radius: 50%;
   background-color: #c4c4c4;
   margin-right: 8px;
@@ -52,6 +58,7 @@ const CarInfo = styled.div`
 `;
 
 const Date = styled.div`
+  font-size: 14px;
   color: #999;
 `;
 
@@ -69,11 +76,10 @@ const Tag = styled.span`
 
 const VideoContainer = styled.div`
   width: 100%;
-  margin-bottom: 16px;
 
-  video{
-   width: 100%;
-   height: 100%;
+  video {
+    width: 100%;
+    height: 100%;
   }
 `;
 
@@ -81,15 +87,37 @@ const Content = styled.div`
   font-size: 17px;
   line-height: 1.5;
   color: #333;
+  border-top: 1px solid #e0e0e0;
+  padding-top: 15px;
+`;
+
+const HeartButton = styled.button`
+  margin: 5px 0px 5px auto;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 24px;
+  color: #666;
+  display: flex;
+  justify-content: flex-end;
+
+  &:hover {
+    color: #666;
+  }
 `;
 
 const DashCamContent: React.FC = () => {
+   const [isLiked, setIsLiked] = useState(false);
+
+   const toggleLike = () => {
+      setIsLiked(!isLiked);
+   };
+
    return (
       <Container>
-         <Body>
+         <Title>
             <h2>과실 비율이 어떻게 되나요?</h2>
-         </Body>
-         <Divider />
+         </Title>
          <Header>
             <User>
                <Avatar />
@@ -111,16 +139,21 @@ const DashCamContent: React.FC = () => {
                   <source src="/images/example_video.mp4" type="video/mp4" />
                </video>
             </VideoContainer>
+            <HeartButton onClick={toggleLike}>
+               {isLiked ? <FaHeart /> : <FaRegHeart />}
+            </HeartButton>
             <Content>
                지난주말에 장인어른이 당한 사고 내용입니다
                접합 차량과 같은방향으로 진행중에 가해 차량이 후진을 해서 급브레이크로 경고 했지만
-               그대로 박았습니다. 블박영상에는 충격이 전해지지만는데, 원본영상에서는 흔들리는 영상이 있습니다(원본은 아직 못올렸네요)
+               그대로 박았습니다. 블박영상에는 충격이 전해지지만는데, 원본영상에서는 흔들리는 영상이
+               있습니다(원본은 아직 못올렸네요)
 
-               장마철 사고이기도해서 그냥 넘어가려고 했는데 내리자마자 "안박았다 왜그러냐" 란 식이었습니다
-               사과하면 그냥 넘어가려고 했으나 이렇게 무시를 하니가
-               "그럼 미안한데 블박보죠" 했다는데요.
+               장마철 사고이기도해서 그냥 넘어가려고 했는데 내리자마자 "안박았다 왜그러냐" 란
+               식이었습니다 사과하면 그냥 넘어가려고 했으나 이렇게 무시를 하니가 "그럼 미안한데
+               블박보죠" 했다는데요.
 
-               바쁨바쁜 우선 연락처만 받고 자리에 이동하였고 장인어른이 이런상황이면 어쩌할줄 재서 거세게 차주분과 제가 연락을 해보았는데 엄한무인이라고요
+               바쁨바쁜 우선 연락처만 받고 자리에 이동하였고 장인어른이 이런상황이면 어쩌할줄 재서
+               거세게 차주분과 제가 연락을 해보았는데 엄한무인이라고요
             </Content>
          </Body>
       </Container>
