c4620f67a9d16ffc6118bec6721f7b2650306eef
박근석 <parkrootseok@gmail.com>
2024-07-27 20:59:28 +0900

    rafact: 게시물 목록 조회 (#43)
    - N+1 해결
      - 멤버에 대한 정보도 하나의 쿼리로 해결

--- backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/repository/LeagueBoardRepository.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/repository/LeagueBoardRepository.java
@@ -5,10 +5,12 @@ import com.luckvicky.blur.domain.leagueboard.model.entity.LeagueBoard;
 import com.luckvicky.blur.global.enums.status.ActivateStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeagueBoardRepository extends JpaRepository<LeagueBoard, Long> {
 
+    @EntityGraph(attributePaths = "member")
     Page<LeagueBoard> findAllByLeagueAndStatus(League league, Pageable pageable, ActivateStatus status);
 
 }
