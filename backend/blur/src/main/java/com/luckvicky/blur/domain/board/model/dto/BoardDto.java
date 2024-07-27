feaa247efd14f3bb125e7091dc3092a956fb4b6f
박근석 <parkrootseok@gmail.com>
2024-07-28 01:06:15 +0900

    조회 관련 기능 리팩토링 (#43)
    Former-commit-id: f0da0aa13a1df985851dff35569b0524529fbebf

--- backend/blur/src/main/java/com/luckvicky/blur/domain/board/model/dto/BoardDto.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/board/model/dto/BoardDto.java
@@ -1,14 +1,20 @@
 package com.luckvicky.blur.domain.board.model.dto;
 
 import com.luckvicky.blur.domain.member.model.MemberDto;
+import com.luckvicky.blur.domain.member.model.SimpleMemberDto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(name = "게시물 정보")
 public class BoardDto {
 
@@ -16,7 +22,7 @@ public class BoardDto {
     private UUID id;
 
     @Schema(description = "사용자 정보")
-    private MemberDto member;
+    private SimpleMemberDto member;
 
     @Schema(description = "제목")
     private String title;
--- backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/repository/LeagueBoardRepository.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/repository/LeagueBoardRepository.java
@@ -2,11 +2,15 @@ package com.luckvicky.blur.domain.leagueboard.repository;
 
 import com.luckvicky.blur.domain.league.model.entity.League;
 import com.luckvicky.blur.domain.leagueboard.model.entity.LeagueBoard;
-import java.util.List;
+import com.luckvicky.blur.global.enums.status.ActivateStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeagueBoardRepository extends JpaRepository<LeagueBoard, Long> {
 
-    List<LeagueBoard> findByLeague(League league);
+    @EntityGraph(attributePaths = "member")
+    Page<LeagueBoard> findAllByLeagueAndStatus(League league, Pageable pageable, ActivateStatus status);
 
 }
