f9c0cabc2ec63ee0064d299061e46c53dab0409c
최은혜 <antlf1104@naver.com>
2024-07-28 23:49:56 +0900

    feat : 채널 게시글 생성(#35)

--- backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/controller/ChannelBoardController.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/controller/ChannelBoardController.java
@@ -4,11 +4,14 @@ import com.luckvicky.blur.domain.board.model.dto.BoardDetailDto;
 import com.luckvicky.blur.domain.board.model.dto.BoardDto;
 import com.luckvicky.blur.domain.board.model.dto.response.BoardDetailResponse;
 import com.luckvicky.blur.domain.board.service.BoardService;
+import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardDto;
 import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardListDto;
+import com.luckvicky.blur.domain.channelboard.model.dto.request.ChannelBoardCreateRequest;
 import com.luckvicky.blur.domain.channelboard.model.dto.response.ChannelBoardListResponse;
 import com.luckvicky.blur.domain.channelboard.service.ChannelBoardService;
 import com.luckvicky.blur.domain.comment.model.dto.CommentDto;
 import com.luckvicky.blur.domain.comment.model.dto.response.CommentListResponse;
+import com.luckvicky.blur.domain.leagueboard.model.dto.request.LeagueBoardCreateRequest;
 import com.luckvicky.blur.domain.leagueboard.model.dto.response.LeagueBoardListResponse;
 import com.luckvicky.blur.global.model.dto.Result;
 import com.luckvicky.blur.global.util.ResponseUtil;
@@ -20,12 +23,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -39,6 +40,33 @@ public class ChannelBoardController {
 
     private final ChannelBoardService channelBoardService;
     private final BoardService boardService;
+
+
+    @Operation(summary = "채널 게시글 생성 API")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "게시글 생성 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "게시글 생성 실패"
+            )
+    })
+    @Parameter(name = "channelId", description = "채널 고유 식별값", in = ParameterIn.PATH)
+    @PostMapping
+    public ResponseEntity createChannelBoard(
+            @PathVariable(name = "channelId") UUID channelId,
+            @Valid @RequestBody ChannelBoardCreateRequest request
+    ) {
+        ChannelBoardDto createdBoard = channelBoardService.createChannelBoard(channelId, request);
+        return ResponseUtil.created(
+                Result.<ChannelBoardDto>builder()
+                        .data(createdBoard)
+                        .build()
+        );
+    }
+
 
     @Operation(
             summary = "채널 게시글 목록 조회 API",
@@ -114,38 +142,4 @@ public class ChannelBoardController {
 
     }
 
-    @Operation(
-            summary = "채널 게시글 댓글 목록 조회 API",
-            description = "특정 게시글에 작성된 댓글 목록을 조회한다."
-    )
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "조회 완료",
-                    content = @Content(schema = @Schema(implementation = CommentListResponse.class))
-            ),
-            @ApiResponse(
-                    responseCode = "204",
-                    description = "조회 완료 (단, 데이터 없음)"
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "존재하지 않는 게시글"
-            )
-    })
-    @Parameter(name = "boardId", description = "게시글 고유 식별값", in = ParameterIn.PATH)
-    @GetMapping("/{boardId}/comments")
-    public ResponseEntity getComments(
-            @PathVariable(name = "boardId") UUID boardId
-    ) {
-
-        List<CommentDto> boardDetail = boardService.getComments(boardId);
-
-        return ResponseUtil.ok(
-                Result.builder()
-                        .data(CommentListResponse.of(boardDetail))
-                        .build()
-        );
-
-    }
 }
--- backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/service/ChannelBoardServiceImpl.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/service/ChannelBoardServiceImpl.java
@@ -8,7 +8,10 @@ import com.luckvicky.blur.domain.channel.model.entity.Channel;
 import com.luckvicky.blur.domain.channel.repository.ChannelRepository;
 import com.luckvicky.blur.domain.channelboard.exception.NotExistChannelException;
 import com.luckvicky.blur.domain.channelboard.mapper.ChannelBoardMapper;
+import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardDto;
 import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardListDto;
+import com.luckvicky.blur.domain.channelboard.model.dto.ChannelBoardMentionDto;
+import com.luckvicky.blur.domain.channelboard.model.dto.request.ChannelBoardCreateRequest;
 import com.luckvicky.blur.domain.channelboard.model.entity.ChannelBoard;
 import com.luckvicky.blur.domain.channelboard.model.entity.ChannelBoardMention;
 import com.luckvicky.blur.domain.channelboard.repository.ChannelBoardMentionRepository;
@@ -17,6 +20,12 @@ import com.luckvicky.blur.domain.comment.model.dto.CommentDto;
 import com.luckvicky.blur.domain.comment.model.entity.Comment;
 import com.luckvicky.blur.domain.comment.model.entity.CommentType;
 import com.luckvicky.blur.domain.comment.repository.CommentRepository;
+import com.luckvicky.blur.domain.dashcamboard.model.entity.DashcamMention;
+import com.luckvicky.blur.domain.league.exception.NotExistLeagueException;
+import com.luckvicky.blur.domain.league.model.entity.League;
+import com.luckvicky.blur.domain.league.repository.LeagueRepository;
+import com.luckvicky.blur.domain.member.model.entity.Member;
+import com.luckvicky.blur.domain.member.repository.MemberRepository;
 import com.luckvicky.blur.global.enums.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -33,9 +42,11 @@ public class ChannelBoardServiceImpl implements ChannelBoardService{
 
     private final ModelMapper mapper;
     private final ChannelBoardMapper channelBoardMapper;
+    private final MemberRepository memberRepository;
     private final ChannelRepository channelRepository;
     private final ChannelBoardRepository channelBoardRepository;
     private final ChannelBoardMentionRepository channelBoardMentionRepository;
+    private final LeagueRepository leagueRepository;
     private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
 
@@ -81,6 +92,33 @@ public class ChannelBoardServiceImpl implements ChannelBoardService{
                 .map(comment -> mapper.map(comment, CommentDto.class))
                 .collect(Collectors.toList());
 
+    }
+
+    @Override
+    public ChannelBoardDto createChannelBoard(UUID channelId, ChannelBoardCreateRequest request) {
+        Channel channel = channelRepository.findById(channelId)
+                .orElseThrow(() -> new NotExistChannelException(ErrorCode.NOT_EXIST_CHANNEL));
+
+        Member member = memberRepository.getOrThrow(request.memberId());
+
+        ChannelBoard channelBoard = request.toEntity(channel, member);
+        channelBoard = channelBoardRepository.save(channelBoard);
+
+        List<League> mentionedLeagues = leagueRepository.findAllByNameIn(request.mentionedLeagueNames());
+
+        if(mentionedLeagues.size() != request.mentionedLeagueNames().size()){
+            throw new NotExistLeagueException(ErrorCode.NOT_EXIST_LEAGUE);
+        }
+
+        for (League league : mentionedLeagues) {
+            ChannelBoardMention mention = ChannelBoardMention.builder()
+                    .channelBoard(channelBoard)
+                    .league(league)
+                    .build();
+            channelBoardMentionRepository.save(mention);
+        }
+
+        return channelBoardMapper.toChannelBoardDto(channelBoard);
     }
 
 
