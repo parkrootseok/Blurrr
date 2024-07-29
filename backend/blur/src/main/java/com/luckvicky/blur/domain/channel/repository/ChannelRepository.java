3efa128950f3aafac83fb1131feaead5c181c8b8
최은혜 <antlf1104@naver.com>
2024-07-30 06:34:28 +0900

    feat : 사용자가 팔로우한 채널 목록 조회 (#35)
    Former-commit-id: 422d430493e0aa495e50d3be72ad5b17c6c31d3e

--- backend/blur/src/main/java/com/luckvicky/blur/domain/channel/repository/ChannelRepository.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channel/repository/ChannelRepository.java
@@ -4,8 +4,10 @@ import com.luckvicky.blur.domain.channel.model.entity.Channel;
 import com.luckvicky.blur.domain.channelboard.exception.NotExistChannelException;
 import com.luckvicky.blur.global.enums.code.ErrorCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -14,5 +16,8 @@ public interface ChannelRepository extends JpaRepository<Channel, UUID> {
     default Channel getOrThrow(UUID id){
         return findById(id).orElseThrow(() -> new NotExistChannelException(ErrorCode.NOT_EXIST_CHANNEL));
     }
+
+    @Query("SELECT c.id FROM Channel c")
+    List<UUID> findAllChannelIds();
 
 }
