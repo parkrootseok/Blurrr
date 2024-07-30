3356359dc3172c834b528765551527768eb41ec5
조민주 <pida6716@gmail.com>
2024-07-30 14:58:44 +0900

    feat: 블랙박스 목록 api에 맞게 데이터 변경 (#49)

--- frontend/src/app/(channel)/channels/dashcam/page.tsx
+++ frontend/src/app/(channel)/channels/dashcam/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useRouter } from "next/navigation";
 import DashCamCard from '@/components/channel/dashcam/DashCamCard';
 import PostTitle from '@/components/channel/PostTitle';
-import dummy from "@/db/data.json";
+import { fetchDashCams } from '@/api/channel';
+import { DashCams } from '@/types/channelType';
 
 const Container = styled.div``;
 
@@ -14,31 +15,45 @@ const CardGrid = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 20px;
 
-  /* 핸드폰 설정 */
   @media (max-width: 480px) {
     grid-template-columns: repeat(1, minmax(200px, 1fr));
   }
 
-  /* 태블릿 크기 이상 설정 */
   @media (min-width: 768px) {
     grid-template-columns: repeat(2, minmax(200px, 1fr));
   }
 
-  /* 데스크탑 크기 이상 설정 */
   @media (min-width: 1024px) {
     grid-template-columns: repeat(3, minmax(200px, 1fr));
   }
 
-  /* 큰 데스크탑 크기 이상 설정 */
   @media (min-width: 1440px) {
     grid-template-columns: repeat(4, minmax(200px, 1fr));
   }
 `;
 
 const DashCam: React.FC = () => {
-   const router = useRouter(); // useRouter 훅 사용
+   const [dashCams, setDashCams] = useState<DashCams[]>([]);
+   const router = useRouter();
 
-   const handleCardClick = (id: number) => {
+   useEffect(() => {
+      const loadData = async () => {
+         try {
+            const data = await fetchDashCams(0, 'createdAt');
+            if (Array.isArray(data)) {
+               setDashCams(data); // 데이터가 배열인지 확인
+            } else {
+               console.error('API did not return an array:', data);
+            }
+         } catch (error) {
+            console.error('Failed to load dash cam data:', error);
+         }
+      };
+
+      loadData();
+   }, []);
+
+   const handleCardClick = (id: string) => {
       router.push(`/channels/dashcam/${id}`);
    };
 
@@ -46,15 +61,10 @@ const DashCam: React.FC = () => {
       <Container>
          <PostTitle channel="dashcam" title="다양한 블랙박스 영상을 보고 직접 투표하자" />
          <CardGrid>
-            {dummy.dashCamList.map((item) => (
-               <div key={item.id} onClick={() => handleCardClick(item.id)}>
+            {dashCams.map((dashCam) => (
+               <div key={dashCam.id} onClick={() => handleCardClick(dashCam.id)}>
                   <DashCamCard
-                     thumbnail={item.thumbnail}
-                     tags={item.tags}
-                     title={item.title}
-                     viewer={item.viewer}
-                     likes={item.likes}
-                     createdDate={item.createdDate}
+                     dashCamTitle={dashCam}
                   />
                </div>
             ))}
