833f2e6fa1438ed69f6db48595f2dfa7afa3c2ca
조민주 <pida6716@gmail.com>
2024-07-30 18:00:07 +0900

    feat: 채널 게시물 목록 api (#49)
    Former-commit-id: be2649da4c372b768916f307896531a2f68f4c7b

--- frontend/src/components/channel/board/ChannelBoardList.tsx
+++ frontend/src/components/channel/board/ChannelBoardList.tsx
@@ -1,25 +1,32 @@
 import styled from "styled-components";
 
-import dummy from "@/db/leaguechannelData.json";
+import React, { useEffect, useState } from 'react';
 import ChannelBoardListItem from "@/components/channel/board/ChannelBoardListItem";
+import { fetchPosts } from '@/api/channel';
+import { PostData } from '@/types/channelType';
 
 const ChannelBoardList = () => {
+  const [Posts, setPosts] = useState<PostData[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await fetchPosts('11ef4e3b-36d9-9292-a7f5-1d7ed3babb22', 0, 'TIME');
+        setPosts(data);
+      } catch (error) {
+        console.error('Failed to load dash cam data:', error);
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
     <ChannelList>
-        {dummy.Lists.map((lst,index) => (
-            <ChannelBoardListItem
-                key={index}
-                mentions={lst.mentions}
-                title={lst.title}
-                texts={lst.texts}
-                writer={lst.writer}
-                writertag={lst.writertag}
-                times={lst.times}
-                likes={lst.likes}
-                comments={lst.comments}
-            />
-        ))}
-        
+      {Posts.map((post) => (
+        <ChannelBoardListItem key={post.id} post={post} />
+      ))}
+
     </ChannelList>
   );
 };
