4cbbacb1bb6f92c953f6e57e5326d1550f36e452
조민주 <pida6716@gmail.com>
2024-07-31 10:29:20 +0900

    feat: 채널 게시글 api 연결 (#49)
    Former-commit-id: 56f37e7d2627520995b42fccdf1a80cbb32887f4

--- frontend/src/components/channel/board/ChannelBoardList.tsx
+++ frontend/src/components/channel/board/ChannelBoardList.tsx
@@ -4,9 +4,11 @@ import React, { useEffect, useState } from 'react';
 import ChannelBoardListItem from "@/components/channel/board/ChannelBoardListItem";
 import { fetchPosts } from '@/api/channel';
 import { PostData } from '@/types/channelType';
+import { useRouter } from "next/navigation";
 
 const ChannelBoardList = () => {
   const [Posts, setPosts] = useState<PostData[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const loadData = async () => {
@@ -21,10 +23,19 @@ const ChannelBoardList = () => {
     loadData();
   }, []);
 
+  const handlePostClick = (boardId: string) => {
+    router.push(`/channels/11ef4e3b-36d9-9292-a7f5-1d7ed3babb22/${boardId}`);
+  };
+
   return (
     <ChannelList>
       {Posts.map((post) => (
-        <ChannelBoardListItem key={post.id} post={post} />
+        <ChannelBoardListItem
+          key={post.board.id}
+          post={post.board}
+          mentions={post.mentionedLeagues}
+          onClick={() => handlePostClick(post.board.id)}
+        />
       ))}
 
     </ChannelList>
