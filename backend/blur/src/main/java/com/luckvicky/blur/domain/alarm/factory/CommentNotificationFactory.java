0fac19e832b3427b2d671c700035092ba1b3a0f9
전상현 <breakking@naver.com>
2024-08-02 08:46:28 +0900

    refact: Bean 등록 시 factory 생성

--- backend/blur/src/main/java/com/luckvicky/blur/domain/alarm/factory/CommentNotificationFactory.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/alarm/factory/CommentNotificationFactory.java
@@ -2,11 +2,7 @@ package com.luckvicky.blur.domain.alarm.factory;
 
 import com.luckvicky.blur.domain.alarm.factory.notification.CommentNotification;
 import com.luckvicky.blur.domain.alarm.factory.notification.Notification;
-import com.luckvicky.blur.domain.alarm.model.entity.AlarmType;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
-@Component(value = "COMMENT")
 public class CommentNotificationFactory implements NotificationFactory {
     @Override
     public Notification createNotification(String... args) {
--- backend/blur/src/main/java/com/luckvicky/blur/global/config/NotificationConfig.java
+++ backend/blur/src/main/java/com/luckvicky/blur/global/config/NotificationConfig.java
@@ -2,6 +2,7 @@ package com.luckvicky.blur.global.config;
 
 import com.luckvicky.blur.domain.alarm.factory.CommentNotificationFactory;
 import com.luckvicky.blur.domain.alarm.factory.NotificationFactory;
+import com.luckvicky.blur.domain.alarm.factory.notification.CommentNotification;
 import com.luckvicky.blur.domain.alarm.model.entity.AlarmType;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,13 +15,15 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class NotificationConfig {
 
-//    @Bean
-//    public Map<String, NotificationFactory> factoryMap(
-//            CommentNotificationFactory commentNotificationFactory
-//    ) {
-//        log.info("=======================================");
-//        Map<String, NotificationFactory> factoryMap = new HashMap<>();
-//        factoryMap.put(AlarmType.ADD_COMMENT.getType(), commentNotificationFactory);
-//        return factoryMap;
-//    }
+    @Bean
+    public Map<String, NotificationFactory> factoryMap() {
+        log.info("=======================================");
+        Map<String, NotificationFactory> factoryMap = new HashMap<>();
+        factoryMap.put(AlarmType.ADD_COMMENT.getType(), commentNotificationFactory());
+        return factoryMap;
+    }
+
+    private CommentNotificationFactory commentNotificationFactory() {
+        return new CommentNotificationFactory();
+    }
 }
