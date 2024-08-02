a5a35f30c1252ff9213de5944491866efec737be
조민주 <pida6716@gmail.com>
2024-08-02 18:03:37 +0900

    feat: 내 차 자랑 수정 (#71)
    Former-commit-id: 4ffb6aaf0133775a3c1b27e0981b07dbfb8e32da

--- frontend/src/app/(channel)/channels/boast/page.tsx
+++ frontend/src/app/(channel)/channels/boast/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import BoastCard from '@/components/channel/boast/BoastCard';
 import PostTitle from '@/components/channel/PostTitle';
 import dummy from "@/db/data.json";
+import { fetchPosts } from '@/api/channel';
+import { PostData } from '@/types/channelType';
 
 const CardGrid = styled.div`
   display: grid;
@@ -13,18 +15,54 @@ const CardGrid = styled.div`
 `;
 
 const Boast: React.FC = () => {
+   const [Posts, setPosts] = useState<PostData[]>([]);
+   const [keyword, setKeyword] = useState('');
+   const [sortCriteria, setSortCriteria] = useState('TIME');
+
+   const handleSortChange = (newSort: string) => {
+      // 정렬 기준을 변경하고, API에서 사용할 수 있는 형식으로 변환
+      const criteriaMap: { [key: string]: string } = {
+         '최신순': 'TIME',
+         '댓글수': 'COMMENT',
+         '조회수': 'VIEW',
+         '좋아요': 'LIKE'
+      };
+
+      const newCriteria = criteriaMap[newSort] || 'TIME'; // 매핑되지 않는 경우 기본값 설정
+      setSortCriteria(newCriteria);
+   };
+
+   const handleSearch = (newKeyword: string) => {
+      setKeyword(newKeyword);
+   };
+
+   useEffect(() => {
+      const loadData = async () => {
+         try {
+            const data = await fetchPosts('11ef4e3b-36d9-9292-a7f5-1d7ed3babb22', keyword, 0, sortCriteria);
+            setPosts(data);
+            console.log('Posts loaded:', data);
+         } catch (error) {
+            console.error('Failed to load channel board list data:', error);
+         }
+      };
+
+      loadData();
+   }, [keyword, criteria]);
 
    return (
       <>
          <PostTitle
             channel="boast"
             title="내 차를 자랑해보자"
+            onSearch={handleSearch}
+            onSortChange={handleSortChange}
          />
          <CardGrid>
-            {dummy.carBoast.map((item) => (
-               <div key={item.id} onClick={() => console.log("item pressed")}>
+            {Posts.map((post) => (
+               <div key={post.board.id} onClick={() => console.log("item pressed")}>
                   <BoastCard
-                     thumbnail={item.thumbnail}
+                     thumbnail={post.board.}
                      viewer={item.viewer}
                      likes={item.likes}
                   />
