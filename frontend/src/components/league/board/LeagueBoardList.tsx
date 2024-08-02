c22f09e4c64e5485e314953fd515617296cae82c
김주연 <juyun7908@naver.com>
2024-08-02 16:19:00 +0900

    feat: 리그 페이지 권한 확인
    - 본인이 속한 리그가 아니면 디테일 페이지 들어가지 못하게 막음
    - 본인이 속한 리그가 아니면 write 페이지로 갔을 때 뒤로 보냄

--- frontend/src/components/league/board/LeagueBoardList.tsx
+++ frontend/src/components/league/board/LeagueBoardList.tsx
@@ -5,11 +5,18 @@ import LeagueBoardListItem from "./LeagueBoardListItem";
 import { fetchLeagueBoardList } from "@/api/league";
 import { useEffect, useState } from "react";
 import { boardListProp, LeagueBoardItem } from "@/types/leagueTypes";
+import { useLeagueStore } from "@/store/leagueStore";
 
 const LeagueBoardList = ({ leagueId, boardList }: boardListProp) => {
   const router = useRouter();
+  const { userLeagueList } = useLeagueStore();
 
   const handleCardClick = (id: string) => {
+    const hasAccess = userLeagueList.some((league) => league.id === leagueId);
+    if (!hasAccess) {
+      alert("허용되지 않은 리그입니다.");
+      return;
+    }
     router.push(`${leagueId}/${id}`);
   };
 
