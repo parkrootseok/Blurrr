5f25f7ea1b534871c6d193a4badfd12c6902e219
김주연 <juyun7908@naver.com>
2024-08-02 15:24:36 +0900

    fix: 메인페이지 api파일 수정
    - 메인 페이지 api파일 수정
    
    
    Former-commit-id: 864f35d50c58cc657c80962e67abcb73c50628bb

--- frontend/src/api/mainPage.ts
+++ frontend/src/api/mainPage.ts
@@ -1,9 +1,10 @@
+import { LeagueList } from "@/types/leagueTypes";
 import api from "./index";
 import { HotBoardItem, TodayCarItem } from "@/types/mainPageTypes";
 
 export const fetchHotArticles = async (): Promise<HotBoardItem[]> => {
   try {
-    const response = await api.get(`/v1/boards/hot`);
+    const response = await api.get(`/v1/channels/hot`);
     if (!response.data.data) {
       return [];
     }
@@ -16,7 +17,7 @@ export const fetchHotArticles = async (): Promise<HotBoardItem[]> => {
 
 export const fetchTodayCar = async (): Promise<TodayCarItem> => {
   try {
-    const response = await api.get(`/v1/boards/today/mycar`);
+    const response = await api.get(`/v1/channels/today/mycar`);
     return response.data.data.myCar;
   } catch (error) {
     console.log(error);
@@ -26,7 +27,7 @@ export const fetchTodayCar = async (): Promise<TodayCarItem> => {
 
 export const fetchMyCars = async (): Promise<TodayCarItem[]> => {
   try {
-    const response = await api.get(`/v1/boards/mycar`);
+    const response = await api.get(`/v1/channels/mycar`);
     return response.data.data.boards;
   } catch (error) {
     console.log(error);
@@ -36,10 +37,19 @@ export const fetchMyCars = async (): Promise<TodayCarItem[]> => {
 
 export const fetchDashCams = async (): Promise<TodayCarItem[]> => {
   try {
-    const response = await api.get(`/v1/boards/dashcam`);
+    const response = await api.get(`/v1/channels/dashcam`);
     return response.data.data.boards;
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+};
+
+export const fetchLeagueRanking = async (): Promise<LeagueList[]> => {
+  try {
+    const response = await api.get(`/v1/leagues/ranking`);
+    return response.data.data.leagues;
+  } catch (error) {
     throw error;
   }
 };
