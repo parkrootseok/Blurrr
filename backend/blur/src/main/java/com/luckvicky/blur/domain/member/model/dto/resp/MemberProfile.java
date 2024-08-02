bac5e75944a65c75258701597e2a76e94164f676
전상현 <breakking@naver.com>
2024-08-02 12:40:19 +0900

    refact: 사용자 프로필 정보 isAuth 필드 추가

--- backend/blur/src/main/java/com/luckvicky/blur/domain/member/model/dto/resp/MemberProfile.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/member/model/dto/resp/MemberProfile.java
@@ -2,17 +2,19 @@ package com.luckvicky.blur.domain.member.model.dto.resp;
 
 import com.luckvicky.blur.domain.member.model.entity.Member;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.util.UUID;
 
-
+@Schema(name = "사용자 프로필 정보")
 public record MemberProfile(
         @Schema(description = "이메일")
         String email,
         @Schema(description = "이미지 url")
         String profileUrl,
         @Schema(description = "닉네임")
-        String nickname) {
+        String nickname,
+        @Schema(description = "차량 인증 여부")
+        boolean isAuth
+        ) {
     public static MemberProfile of(Member member) {
-        return new MemberProfile(member.getEmail(), member.getProfileUrl(), member.getNickname());
+        return new MemberProfile(member.getEmail(), member.getProfileUrl(), member.getNickname(), member.isAuth());
     }
 }
