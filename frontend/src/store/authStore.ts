64850df5d4236589e8ccb6d49b52e581c2e03c2f
정승훈 <seunghun311@naver.com>
2024-08-02 11:05:33 +0900

    fix: 로그인 코드 개선 (#56)
    - 로그인 코드 개선
    - 사용자 정보 store 저장
    
    
    Former-commit-id: 119266b22f9b5351b6dd30a10bb247d656f33b8a

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -1,11 +1,22 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+interface User {
+  email: string;
+  profileUrl: string;
+  nickname: string;
+  isAuth: boolean;
+}
+
 interface AuthState {
   accessToken: string | null;
+  refreshToken: string | null;
+  user: User | null;
   isLoggedIn: boolean;
   setAccessToken: (token: string | null) => void;
-  clearAccessToken: () => void;
+  setRefreshToken: (token: string | null) => void;
+  setUser: (user: User | null) => void;
+  clearAuthState: () => void;
   setIsLoggedIn: (status: boolean) => void;
 }
 
@@ -13,14 +24,18 @@ export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       accessToken: null,
+      refreshToken: null,
+      user: null,
       isLoggedIn: false,
       setAccessToken: (token) => set({ accessToken: token, isLoggedIn: !!token }),
-      clearAccessToken: () => set({ accessToken: null, isLoggedIn: false }),
+      setRefreshToken: (token) => set({ refreshToken: token }),
+      setUser: (user) => set({ user }),
+      clearAuthState: () => set({ accessToken: null, refreshToken: null, user: null, isLoggedIn: false }),
       setIsLoggedIn: (status) => set({ isLoggedIn: status }),
     }),
     {
-      name: 'auth-storage', // 로컬 스토리지에 저장될 키
-      getStorage: () => sessionStorage, // 사용할 스토리지 (localStorage 또는 sessionStorage)
+      name: 'auth-storage',
+      getStorage: () => sessionStorage,
     }
   )
 );
