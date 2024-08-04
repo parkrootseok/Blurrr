fd918131f648bb72cee5cc81e6ca52441555f433
박근석 <parkrootseok@gmail.com>
2024-08-04 19:03:30 +0900

    refactor: 분산락 (#87)
    - 분산락을 통해 트랜잭션 내부에 추가 트랜잭션으로 동작하도록 하여 부모 트랜잭션이 끝나기 전에 반드시 추가 트랜잭션이 끝나도록 함
    
    
    Former-commit-id: 34bed08be9a2ac4e3f9e1b56fc3f26e9f07d7a64

--- backend/blur/src/main/java/com/luckvicky/blur/infra/redisson/DistributedLockAspect.java
+++ backend/blur/src/main/java/com/luckvicky/blur/infra/redisson/DistributedLockAspect.java
@@ -10,7 +10,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -19,39 +18,48 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class DistributedLockAspect {
 
+    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
     private final RedissonClient redissonClient;
+    private final DistributedLockTransaction distributedLockTransaction;
 
     @Around("@annotation(com.luckvicky.blur.infra.redisson.DistributedLock)")
-    public void DistributedLock(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object DistributedLock(ProceedingJoinPoint joinPoint) throws Throwable {
 
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
         DistributedLock distributedLock = method.getAnnotation(DistributedLock.class);
 
-        String lockKey = new StringBuilder()
+        String key = new StringBuilder()
+                .append(REDISSON_LOCK_PREFIX)
                 .append(method.getName())
                 .append(CustomSpringELParser.getDynamicValue(
                         signature.getParameterNames(), joinPoint.getArgs(), distributedLock.value()
                 ))
                 .toString();
 
-        RLock rLock = redissonClient.getLock(lockKey);
+        RLock rLock = redissonClient.getLock(key);
 
         try {
 
             boolean lockable = rLock.tryLock(distributedLock.waitTime(), distributedLock.occupancyTime(), TimeUnit.MICROSECONDS);
 
             if (!lockable) {
-                log.info("Fail to occupy lock={}", lockKey);
-                return;
+                log.info("Fail to occupy lock={}", key);
+                throw new InterruptedException();
             }
 
-            joinPoint.proceed();
+            return distributedLockTransaction.proceed(joinPoint);
 
         } catch (InterruptedException e) {
-            throw e;
+            throw new InterruptedException();
         } finally {
-            rLock.unlock();;
+
+            try {
+                rLock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                log.info("Redisson Lock Already UnLock {} {}", method.getName(), key);
+            }
+
         }
 
     }
