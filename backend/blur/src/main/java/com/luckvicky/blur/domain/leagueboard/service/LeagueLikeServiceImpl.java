a155ffdc8fcf2a92a4163fef3a1c8e143f03df7e
박근석 <parkrootseok@gmail.com>
2024-08-04 19:04:34 +0900

    refactor: 좋아요 증감 분산락 적용 (#87)
    - 0보다 작아질 경우 0으로 초기화되도록 추가
    
    
    Former-commit-id: 9845992d0b65bddf21521ea4a266b9191dca32b2

--- backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/service/LeagueLikeServiceImpl.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/service/LeagueLikeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.luckvicky.blur.domain.leagueboard.service;
 
+import com.luckvicky.blur.domain.board.exception.NotExistBoardException;
 import com.luckvicky.blur.domain.board.model.entity.Board;
 import com.luckvicky.blur.domain.leagueboard.model.entity.LeagueBoard;
 import com.luckvicky.blur.domain.leagueboard.repository.LeagueBoardRepository;
@@ -15,13 +16,12 @@ import com.luckvicky.blur.domain.like.repository.LikeRepository;
 import com.luckvicky.blur.domain.member.model.entity.Member;
 import com.luckvicky.blur.domain.member.repository.MemberRepository;
 import com.luckvicky.blur.infra.redisson.DistributedLock;
-import jakarta.transaction.Transactional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class LeagueLikeServiceImpl implements LeagueLikeService {
 
@@ -31,6 +31,7 @@ public class LeagueLikeServiceImpl implements LeagueLikeService {
     private final LeagueBoardRepository leagueBoardRepository;
 
     @Override
+    @DistributedLock(value = "#boardId")
     public LikeCreateResponse createLike(UUID memberId, UUID boardId) {
 
         Member member = memberRepository.getOrThrow(memberId);
@@ -49,12 +50,13 @@ public class LeagueLikeServiceImpl implements LeagueLikeService {
             throw new FailToDeleteLikeException();
         }
 
-        increaseLikeCount(board);
+        board.increaseLikeCount();
         return LikeCreateResponse.of(board.getLikeCount(), isLike(member, board));
 
     }
 
     @Override
+    @Transactional(readOnly = true)
     public LikeStatusResponse getLikeStatusByBoard(UUID memberId, UUID boardId) {
 
         Member member = memberRepository.getOrThrow(memberId);
@@ -66,10 +68,12 @@ public class LeagueLikeServiceImpl implements LeagueLikeService {
     }
 
     @Override
+    @DistributedLock(value = "#boardId")
     public LikeDeleteResponse deleteLike(UUID memberId, UUID boardId) {
 
         Member member = memberRepository.getOrThrow(memberId);
         LeagueBoard board = leagueBoardRepository.getOrThrow(boardId);
+
         isAllocatedLeague(board, member);
 
         Like findLike = likeRepository.findByMemberAndBoard(member, board)
@@ -78,23 +82,14 @@ public class LeagueLikeServiceImpl implements LeagueLikeService {
         likeRepository.deleteById(findLike.getId());
 
         Boolean isLike = isLike(member, board);
+
         if (isLike) {
             throw new FailToDeleteLikeException();
         }
 
-        decreaseLikeCount(board);
+        board.decreaseLikeCount();
         return LikeDeleteResponse.of(board.getLikeCount(), isLike);
 
-    }
-
-    @DistributedLock(value = "#boardId")
-    private static void increaseLikeCount(Board board) {
-        board.increaseLikeCount();
-    }
-
-    @DistributedLock(value = "#boardId")
-    private static void decreaseLikeCount(Board board) {
-        board.decreaseLikeCount();
     }
 
     private void isAllocatedLeague(LeagueBoard board, Member member) {
