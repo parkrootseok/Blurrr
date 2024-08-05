0ae37f2a2965d502ed9688b887500ccb14e0d5d9
조민주 <pida6716@gmail.com>
2024-08-06 08:55:09 +0900

    fix: mixed content 배포 버그 (#101)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,20 @@
 module.exports = {
-    compiler: {
-      styledComponents: true,
-    },
-  }
+  // reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        // 이 부분을 원하는 경로로 설정합니다. 모든 경로에 적용하려면 아래와 같이 합니다.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "upgrade-insecure-requests",
+          },
+        ],
+      },
+    ];
+  },
+};
