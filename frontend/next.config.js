2778ae0a20316b31c02508227259ad4249646831
김주연 <juyun7908@naver.com>
2024-08-06 12:43:58 +0900

    feat: 일반 채널 pagination 구현(#107)
    - 일반 채널 pagination 구현
    
    
    Former-commit-id: c394dd988f0e28f9227b04ea45a92b3fe8aa6b94

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,18 +4,4 @@ module.exports = {
     styledComponents: true,
   },
   output: "standalone",
-  async headers() {
-    return [
-      {
-        // 이 부분을 원하는 경로로 설정합니다. 모든 경로에 적용하려면 아래와 같이 합니다.
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-        ],
-      },
-    ];
-  },
 };
