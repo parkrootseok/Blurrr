7a9073f8f915cc15d5e5272a9769663eac6cf2cb
박근석 <parkrootseok@gmail.com>
2024-08-07 15:19:37 +0900

    메인 페이지 게시글 목록 조회 (#114)
    Former-commit-id: a7849810b6bc276c9920755656135526f44a2f45

---
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/model/dto/response/HotDashCamListResponse.java
@@ -0,0 +1,31 @@
+package com.luckvicky.blur.domain.channelboard.model.dto.response;
+
+import com.luckvicky.blur.domain.dashcam.model.entity.DashCam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+@Schema(name = "인기 블랙박스 게시물 정보")
+public record HotDashCamListResponse(
+
+        @Schema(description = "고유 식별값")
+        UUID id,
+
+        @Schema(description = "제목")
+        String title,
+
+        @Schema(description = "참여 인원")
+        Long voteCount
+
+) {
+
+        public static HotDashCamListResponse of(DashCam dashCam) {
+                return HotDashCamListResponse.builder()
+                        .id(dashCam.getId())
+                        .title(dashCam.getTitle())
+                        .voteCount(dashCam.getTotalVoteCount())
+                        .build();
+        }
+
+}
