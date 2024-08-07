1688e9a331c1cbdd091204735fbc2715f5e5d4ec
최은혜 <antlf1104@naver.com>
2024-08-07 16:06:54 +0900

    feat : 채널 게시물 목록 조회시, 조회수 / 본문 미리보기 추가(#113)

--- backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/mapper/ChannelBoardMapper.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/channelboard/mapper/ChannelBoardMapper.java
@@ -35,6 +35,13 @@ public class ChannelBoardMapper {
                 .createdAt(channelBoard.getCreatedAt().toString())
                 .commentCount(channelBoard.getCommentCount())
                 .likeCount(channelBoard.getLikeCount())
+                .viewCount(channelBoard.getViewCount())
+                .simpleContent(channelBoard.getContent()
+                        .replaceAll("<[^>]*>", "")
+                        .replaceAll("\\s+", " ")
+                        .trim()
+                        .substring(0, Math.min(30, channelBoard.getContent().length()))
+                        + (channelBoard.getContent().length() > 30 ? "..." : ""))
                 .build();
     }
 
