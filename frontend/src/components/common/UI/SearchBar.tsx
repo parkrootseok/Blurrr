e034298467ee01acc6da7751b857bc9948903afb
김주연 <juyun7908@naver.com>
2024-08-07 13:14:20 +0900

    feat: 리그 게시글 리스트 페이지 UI 변경
    - 리그 게시글 리스트 UI 변경
    - 더보기 탭 UI 변경
    
    
    Former-commit-id: a37663b8c50c6dcb7544a5a08379cd8178d5b0d7

--- frontend/src/components/common/UI/SearchBar.tsx
+++ frontend/src/components/common/UI/SearchBar.tsx
@@ -1,91 +1,7 @@
-// import React, { useState } from "react";
-// import styled from "styled-components";
-// import { HiOutlineSearch } from "react-icons/hi";
-// import { fetchBoardSearch } from "@/api/league";
-
-// interface SearchBarProps {
-//   onSearch: (keyword: string) => void;
-// }
-
-// const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-//   const [keyword, setKeyword] = useState("");
-
-//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setKeyword(e.target.value);
-//   };
-
-//   const handleSubmit = () => {
-//     // if (!keyword.trim()) return;
-//     onSearch(keyword);
-//     setKeyword("");
-//   };
-//   return (
-//     <Container>
-//       <SearchContainer>
-//         <SearchIcon>
-//           <HiOutlineSearch />
-//         </SearchIcon>
-//         <SearchInput
-//           type="text"
-//           placeholder="Search"
-//           value={keyword}
-//           onChange={handleChange}
-//         />
-//       </SearchContainer>
-//       <button onClick={handleSubmit}>검색</button>
-//     </Container>
-//   );
-// };
-
-// export default SearchBar;
-
-// const Container = styled.div`
-//   width: 40%;
-//   display: flex;
-//   align-items: center;
-
-//   button {
-//     padding: 10px; /* 넉넉한 패딩을 주어 버튼의 높이를 조정 */
-//     border-radius: 5px;
-//     border: 1px solid #ddd;
-//     margin-left: 10px; /* 검색 창과 버튼 사이의 간격 조정 */
-//     background-color: white; /* 버튼 배경색 추가 */
-//     cursor: pointer; /* 커서 포인터 추가 */
-//     font-size: 1rem; /* 글꼴 크기 조정 */
-//     min-width: 64px;
-//   }
-// `;
-
-// const SearchContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   border: 1px solid #ddd;
-//   border-radius: 50px;
-//   padding: 5px 10px;
-//   max-width: 600px;
-//   width: 100%;
-// `;
-
-// const SearchIcon = styled.span`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   font-size: 1.2rem;
-//   color: #aaa;
-//   margin-right: 10px;
-// `;
-
-// const SearchInput = styled.input`
-//   border: none;
-//   outline: none;
-//   flex: 1;
-//   padding: 5px 10px;
-//   font-size: 1rem;
-//   border-radius: 50px;
-// `;
 import React, { useState } from "react";
 import styled from "styled-components";
 import { HiOutlineSearch } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 interface SearchBarProps {
   onSearch: (keyword: string) => void;
@@ -100,8 +16,10 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault(); // 폼 제출 기본 동작 방지
-    // if (!keyword.trim()) return;
     onSearch(keyword);
+  };
+
+  const clearInput = () => {
     setKeyword("");
   };
 
@@ -118,8 +36,13 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
             value={keyword}
             onChange={handleChange}
           />
+          {keyword && (
+            <ClearIcon onClick={clearInput}>
+              <IoClose />
+            </ClearIcon>
+          )}
         </SearchContainer>
-        <button type="submit">검색</button>
+        <SearchButton type="submit">검색</SearchButton>
       </SearchForm>
     </Container>
   );
@@ -137,26 +60,17 @@ const SearchForm = styled.form`
   width: 100%;
   display: flex;
   align-items: center;
-
-  button {
-    padding: 10px; /* 넉넉한 패딩을 주어 버튼의 높이를 조정 */
-    border-radius: 5px;
-    border: 1px solid #ddd;
-    margin-left: 10px; /* 검색 창과 버튼 사이의 간격 조정 */
-    background-color: white; /* 버튼 배경색 추가 */
-    cursor: pointer; /* 커서 포인터 추가 */
-    font-size: 1rem; /* 글꼴 크기 조정 */
-    min-width: 64px;
-  }
 `;
 
 const SearchContainer = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   border: 1px solid #ddd;
   border-radius: 50px;
   padding: 5px 10px;
   max-width: 600px;
+  max-height: 28px;
   width: 100%;
 `;
 
@@ -174,6 +88,29 @@ const SearchInput = styled.input`
   outline: none;
   flex: 1;
   padding: 5px 10px;
-  font-size: 1rem;
+  font-size: 14px;
   border-radius: 50px;
+`;
+
+const ClearIcon = styled.span`
+  position: absolute;
+  right: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.2rem;
+  color: #aaa;
+  cursor: pointer;
+`;
+
+const SearchButton = styled.button`
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin-left: 10px;
+  background-color: white;
+  cursor: pointer;
+  font-size: 14px;
+  min-width: 50px;
+  max-height: 38px;
 `;
--- frontend/src/components/league/tab/MoreBrandTab.tsx
+++ frontend/src/components/league/tab/MoreBrandTab.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes, css } from "styled-components";
 import Link from "next/link";
 
 import { moreTabProps, TabButtonProps } from "@/types/leagueTypes";
@@ -56,12 +56,12 @@ export default function MoreBrandTab({
 }
 
 const TabContent = styled.div`
-  padding: 20px 0;
+  margin: 30px 0;
 `;
 
 const MoreTabsButton = styled.button`
   border: none;
-  padding: 10px 0;
+  padding: 10px;
   cursor: pointer;
   border-radius: 20px;
   color: #333;
@@ -72,22 +72,47 @@ const MoreTabsButton = styled.button`
   margin: 0;
 `;
 
-const MoreTabsContainer = styled.div.attrs<{ $isOpen: boolean }>((props) => ({
-  style: {
-    maxHeight: props.$isOpen ? "170px" : "0",
-    padding: props.$isOpen ? "10px" : "0",
-    display: props.$isOpen ? "flex" : "none",
-  },
-}))<{ $isOpen: boolean }>`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+`;
+
+const MoreTabsContainer = styled.div<{ $isOpen: boolean }>`
   overflow-y: auto;
   transition: max-height 0.3s ease, padding 0.3s ease;
   flex-wrap: wrap;
-  border: 1px solid #ccc;
+  border: ${(props) => (props.$isOpen ? "1px solid #ccc" : "none")};
   border-radius: 8px;
   margin-bottom: 20px;
   margin-left: auto;
   justify-content: center;
   align-items: center;
+  max-height: ${(props) => (props.$isOpen ? "170px" : "0")};
+  padding: ${(props) => (props.$isOpen ? "10px" : "0")};
+  display: flex;
+  animation: ${(props) => (props.$isOpen ? fadeIn : fadeOut)} 0.3s ease;
+  ${(props) =>
+    !props.$isOpen &&
+    css`
+      overflow: hidden;
+    `}
 
   &::-webkit-scrollbar {
     width: 10px;
@@ -107,17 +132,20 @@ const MoreTabsContainer = styled.div.attrs<{ $isOpen: boolean }>((props) => ({
 const MoreTabButton = styled(Link)<TabButtonProps>`
   background: ${(props) => (props.$isActive ? "#FFEDD5" : "none")};
   border: none;
-  padding: 10px;
+  padding: 10px 15px;
   cursor: pointer;
   font-size: 14px;
   margin: 5px;
   border: 1px solid #e0e0e0;
   border-radius: 8px;
-  width: 100px;
+  min-width: 100px;
   text-align: center;
   font-weight: ${(props) => (props.$isActive ? "bold" : "normal")};
   color: ${(props) => (props.$isActive ? "#F97316" : "#333")};
   text-decoration: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   &:hover {
     background: #f97316;
@@ -131,11 +159,6 @@ const HeaderContainer = styled.div`
   align-items: center;
 `;
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
 const Title = styled.h1`
   font-size: 24px;
   font-weight: bold;
@@ -145,8 +168,8 @@ const Title = styled.h1`
 const TitleContainer = styled.div``;
 
 const Subtitle = styled.p`
-  color: ${({ theme }) => theme.colors.subDiscription};
+  color: #999;
   font-size: 14px;
   margin: 0;
-  margin-bottom: 20px;
+  margin-bottom: 14px;
 `;
