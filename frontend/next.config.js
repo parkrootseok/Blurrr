d0f9730195f5f5b22ebe517b8b48e1290a79a501
juyun <juyun7908@naver.com>
2024-08-09 00:24:50 +0900

    feat: not-found 페이지 추가(#100)
    - not-found 페이지 추가
    - config 파일 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  // reactStrictMode: false,
+  // 기타 설정
   compiler: {
     styledComponents: true,
   },
   output: "standalone",
+
+  // ESLint 빌드 제외 설정
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
