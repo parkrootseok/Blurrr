88306bd562ef6da75170b060e1e206a15d5e1d07
전상현 <breakking@naver.com>
2024-08-08 15:24:21 +0900

    refact: AuthCodeEmailFormFactory 변경
    Former-commit-id: f902b6c95e15b4635c09a50b761245edaccb362b

--- backend/blur/src/main/java/com/luckvicky/blur/infra/mail/service/AuthCodeEmailFormFactory.java
+++ backend/blur/src/main/java/com/luckvicky/blur/infra/mail/service/AuthCodeEmailFormFactory.java
@@ -1,11 +1,12 @@
 package com.luckvicky.blur.infra.mail.service;
 
 import com.luckvicky.blur.infra.mail.model.EmailForm;
+import com.luckvicky.blur.infra.mail.model.EmailFormData;
 
 public class AuthCodeEmailFormFactory implements EmailFormFactory{
 
     @Override
-    public EmailForm createEmailForm() {
+    public EmailForm createEmailForm(EmailFormData formData) {
         return null;
     }
 }
