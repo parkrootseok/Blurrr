fcdcd6873de36eb728e37ef8f9443331a01e5a7a
조민주 <pida6716@gmail.com>
2024-08-09 21:53:31 +0900

    refactor: 태그 컴포넌트 분리 (#118)

--- frontend/src/app/(channel)/channels/[channelId]/write/page.tsx
+++ frontend/src/app/(channel)/channels/[channelId]/write/page.tsx
@@ -1,90 +1,26 @@
 "use client";
 
-import React, { useState, useRef, useEffect, KeyboardEvent, useCallback } from "react";
+import React, { useState } from "react";
 import { useRouter, useParams } from "next/navigation";
-import styled from 'styled-components';
-import QuillEditor from '@/components/channel/board/QuillEditor';
-import { fetchPostWrite, fetchTags, fetchBoastWrite } from '@/api/channel';
-import { Mentioned } from '@/types/channelType';
+import styled from "styled-components";
+import QuillEditor from "@/components/channel/board/QuillEditor";
+import { fetchPostWrite, fetchBoastWrite } from "@/api/channel";
+import FindTags from "@/components/channel/board/FindTags";
+
 
 export default function WritePage() {
   const boastId = process.env.NEXT_PUBLIC_BOAST_ID;
   const { channelId } = useParams<{ channelId: string }>();
   const router = useRouter();
-  const suggestionsRef = useRef<HTMLDivElement>(null);
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState<string[]>([]);
-  const [tagInput, setTagInput] = useState("");
-  const [tagSuggestions, setTagSuggestions] = useState<Mentioned[]>([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [highlightedIndex, setHighlightedIndex] = useState(-1);
   const [thumbNail, setThumbNail] = useState<string>("");
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (suggestionsRef.current && !suggestionsRef.current.contains(event.target as Node)) {
-        setShowSuggestions(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
   };
-
-  const handleTagInputChange = useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTagInput(e.target.value);
-
-    if (e.target.value.trim()) {
-      try {
-        const suggestions = await fetchTags(e.target.value.trim());
-        setTagSuggestions(suggestions || []);
-        setShowSuggestions(true);
-        console.log("Tag suggestions:", suggestions);
-      } catch (error) {
-        console.error("Error fetching tag suggestions:", error);
-        setTagSuggestions([]);
-      }
-    } else {
-      setTagSuggestions([]);
-      setShowSuggestions(false);
-    }
-  }, []);
-
-  const handleTagKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      if (highlightedIndex >= 0 && tagSuggestions.length > 0) {
-        const selectedTag = tagSuggestions[highlightedIndex];
-        if (tags.length < 3 && !tags.some(tag => tag === selectedTag.name)) {
-          setTags([...tags, selectedTag.name]);
-          setTagInput("");
-          setShowSuggestions(false);
-        }
-      } else if (tagInput.trim() && tags.length < 3 && !tags.some(tag => tag === tagInput.trim())) {
-        setTags([...tags, tagInput.trim()]);
-        setTagInput("");
-        setShowSuggestions(false);
-      }
-    } else if (e.key === "ArrowDown") {
-      e.preventDefault();
-      setHighlightedIndex((prevIndex) => (prevIndex + 1) % tagSuggestions.length);
-    } else if (e.key === "ArrowUp") {
-      e.preventDefault();
-      setHighlightedIndex((prevIndex) => (prevIndex + tagSuggestions.length - 1) % tagSuggestions.length);
-    }
-  };
-
-  const handleTagRemove = useCallback((tagToRemove: string) => {
-    setTags(tags.filter(tag => tag !== tagToRemove));
-  }, [tags]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -106,14 +42,6 @@ export default function WritePage() {
     }
   };
 
-  const handleTagClick = useCallback((suggestion: Mentioned) => {
-    if (tags.length < 3 && !tags.some(tag => tag === suggestion.name)) {
-      setTags([...tags, suggestion.name]);
-      setTagInput("");
-      setShowSuggestions(false);
-    }
-  }, [tags]);
-
   return (
     <Container>
       <PageTitle>게시글 작성</PageTitle>
@@ -124,36 +52,7 @@ export default function WritePage() {
           value={title}
           onChange={handleTitleChange}
         />
-        <TagInputContainer ref={suggestionsRef}>
-          <Input
-            name="tagInput"
-            placeholder="태그를 입력 후 엔터키를 눌러 추가하세요. 최대 3개까지 가능합니다."
-            value={tagInput}
-            onChange={handleTagInputChange}
-            onKeyDown={handleTagKeyDown}
-          />
-          {showSuggestions && tagSuggestions.length > 0 && (
-            <SuggestionsContainer>
-              {tagSuggestions.map((suggestion, index) => (
-                <Suggestion
-                  key={suggestion.name}
-                  onMouseDown={() => handleTagClick(suggestion)}
-                  highlighted={index === highlightedIndex ? 1 : 0}
-                >
-                  {suggestion.name}
-                </Suggestion>
-              ))}
-            </SuggestionsContainer>
-          )}
-          <TagsContainer>
-            {tags.map(tag => (
-              <Tag key={tag}>
-                {tag}
-                <RemoveTagButton onClick={() => handleTagRemove(tag)}>x</RemoveTagButton>
-              </Tag>
-            ))}
-          </TagsContainer>
-        </TagInputContainer>
+        <FindTags tags={tags} setTags={setTags} /> {/* TagsInput 컴포넌트 사용 */}
         <EditorAndButtonContainer>
           <EditorContainer>
             <QuillEditor content={content} setContent={setContent} setThumbNail={setThumbNail} />
@@ -186,57 +85,6 @@ const Input = styled.input`
   border: 1px solid #ddd;
   border-radius: 5px;
   box-sizing: border-box;
-`;
-
-const TagInputContainer = styled.div`
-  width: 100%;
-  max-width: 800px;
-  margin-bottom: 16px;
-  position: relative;
-`;
-
-const SuggestionsContainer = styled.div`
-  position: absolute;
-  font-size: 14px;
-  background-color: white;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  max-height: 150px;
-  overflow-y: auto;
-  width: 100%;
-  z-index: 1000;
-  top: 40px; /* Adjust this value if needed */
-`;
-
-const Suggestion = styled.div<{ highlighted: number }>`
-  padding: 10px;
-  background-color: ${({ highlighted }) => (highlighted === 1 ? '#eee' : 'white')};
-  cursor: pointer;
-
-  &:hover {
-    background-color: #eee;
-  }
-`;
-
-const TagsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-`;
-
-const Tag = styled.div`
-  display: flex;
-  align-items: center;
-  background-color: #eee;
-  border-radius: 5px;
-  padding: 5px 10px;
-  font-size: 15px;
-`;
-
-const RemoveTagButton = styled.span`
-  margin-left: 5px;
-  cursor: pointer;
-  color: #888;
 `;
 
 const EditorAndButtonContainer = styled.div`
