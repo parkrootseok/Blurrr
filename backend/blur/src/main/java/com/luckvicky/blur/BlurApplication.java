8b8632462e67a64d6a1d4dd04df3d13025a37eb8
parkrootseok <parkrootseok@gmail.com>
2024-08-09 14:05:15 +0900

    chore: timeZone 설정

--- backend/blur/src/main/java/com/luckvicky/blur/BlurApplication.java
+++ backend/blur/src/main/java/com/luckvicky/blur/BlurApplication.java
@@ -1,5 +1,8 @@
 package com.luckvicky.blur;
 
+import com.luckvicky.blur.global.util.ClockUtil;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,8 +11,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class BlurApplication {
 
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("---------------------------------------------------");
+		System.out.println(ClockUtil.getLocalDateTime());
+		System.out.println("---------------------------------------------------");
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(BlurApplication.class, args);
+
 	}
 
 }
