f405d0298d427fcee33922a4ecd2c1f0fa3216ff
parkrootseok <parkrootseok@gmail.com>
2024-08-13 09:41:22 +0900

    refactor: 리그 생성 API 삭제

--- backend/blur/src/main/java/com/luckvicky/blur/domain/league/service/LeagueService.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/league/service/LeagueService.java
@@ -1,15 +1,9 @@
 package com.luckvicky.blur.domain.league.service;
 
-import com.luckvicky.blur.domain.league.model.dto.LeagueDto;
-import com.luckvicky.blur.domain.league.model.dto.SimpleLeagueDto;
-import com.luckvicky.blur.domain.league.model.dto.request.LeagueCreateRequest;
 import com.luckvicky.blur.domain.league.model.dto.response.LeagueListResponse;
 import com.luckvicky.blur.domain.league.model.dto.response.LeagueRankingResponse;
-import java.util.List;
 
 public interface LeagueService {
-
-    Boolean createLeague(LeagueCreateRequest request);
 
     LeagueListResponse getLeagues(String leagueType);
 
