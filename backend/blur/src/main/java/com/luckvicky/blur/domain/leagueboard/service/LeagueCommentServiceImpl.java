2e3e005bd8d11746ebc01eb3f884320d702d8549
parkrootseok <parkrootseok@gmail.com>
2024-08-13 10:00:20 +0900

    refactor: 불필요한 import 삭제
    Former-commit-id: 2c69b67c3551d333fc637986fcfb3cef4f90448b

--- backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/service/LeagueCommentServiceImpl.java
+++ backend/blur/src/main/java/com/luckvicky/blur/domain/leagueboard/service/LeagueCommentServiceImpl.java
@@ -4,7 +4,6 @@ import static com.luckvicky.blur.global.enums.code.ErrorCode.FAIL_TO_CREATE_COMM
 
 import com.luckvicky.blur.domain.alarm.model.entity.AlarmType;
 import com.luckvicky.blur.domain.alarm.service.AlarmFacade;
-import com.luckvicky.blur.domain.board.exception.NotExistBoardException;
 import com.luckvicky.blur.domain.board.model.entity.Board;
 import com.luckvicky.blur.domain.comment.exception.FailToCreateCommentException;
 import com.luckvicky.blur.domain.comment.exception.FailToDeleteCommentException;
