32bb3f9f4a4ff84b56d5246cb200772f5ca0492d
김주연 <juyun7908@naver.com>
2024-08-13 13:46:42 +0900

    feat: 리그 권한 확인(#103)
    - 권한 확인

--- frontend/src/api/league.ts
+++ frontend/src/api/league.ts
@@ -104,51 +104,51 @@ export const fetchMentionBoardList = async (
 };
 
 // 리그 게시글 상세 정보를 가져오는 함수
-// export const fetchLeagueDetail = async (
-//   boardId: string
-// ): Promise<BoardDetail> => {
-//   try {
-//     console.log('fetch함수 시작')
-//     const response = await api.get(`/v1/leagues/boards/${boardId}`);
-//     console.log('fetch함수 완료')
-//     return response.data.data;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// };
-import { CancelTokenSource } from "axios";
-
-let cancelTokenSource: CancelTokenSource | null = null;
-
 export const fetchLeagueDetail = async (
   boardId: string
 ): Promise<BoardDetail> => {
-  console.log(cancelTokenSource);
-  if (cancelTokenSource) {
-    cancelTokenSource.cancel("이전 요청이 취소되었습니다.");
-  }
-
-  cancelTokenSource = axios.CancelToken.source();
-
   try {
     console.log("fetch함수 시작");
-    const response = await api.get(`/v1/leagues/boards/${boardId}`, {
-      cancelToken: cancelTokenSource.token,
-    });
+    const response = await api.get(`/v1/leagues/boards/${boardId}`);
     console.log("fetch함수 완료");
     return response.data.data;
   } catch (error) {
-    if (axios.isCancel(error)) {
-      console.log("요청이 취소되었습니다:", error.message);
-    } else {
-      console.log(error);
-      throw error;
-    }
-  } finally {
-    cancelTokenSource = null; // 요청 완료 후 토큰 리셋
+    console.log(error);
+    throw error;
   }
 };
+// import { CancelTokenSource } from "axios";
+
+// let cancelTokenSource: CancelTokenSource | null = null;
+
+// export const fetchLeagueDetail = async (
+//   boardId: string
+// ): Promise<BoardDetail> => {
+//   console.log(cancelTokenSource);
+//   if (cancelTokenSource) {
+//     cancelTokenSource.cancel("이전 요청이 취소되었습니다.");
+//   }
+
+//   cancelTokenSource = axios.CancelToken.source();
+
+//   try {
+//     console.log("fetch함수 시작");
+//     const response = await api.get(`/v1/leagues/boards/${boardId}`, {
+//       cancelToken: cancelTokenSource.token,
+//     });
+//     console.log("fetch함수 완료");
+//     return response.data.data;
+//   } catch (error) {
+//     if (axios.isCancel(error)) {
+//       console.log("요청이 취소되었습니다:", error.message);
+//     } else {
+//       console.log(error);
+//       throw error;
+//     }
+//   } finally {
+//     cancelTokenSource = null; // 요청 완료 후 토큰 리셋
+//   }
+// };
 
 // 리그 게시글 검색 함수
 export const fetchBoardSearch = async (
